79e3084c6331b459aa8bda59d580a65bd668b210
aya ahmed <91829518+aaya-ahmed@users.noreply.github.com>
2023-11-08 20:35:54 +0200

    Update comment.route.js

--- routing/comment.route.js
+++ routing/comment.route.js
@@ -2,9 +2,9 @@ const comment=require("../controller/comment.controller.js")
 const authorization = require("../middleware/authorization.js")
 const router=require("express").Router()
 
-router.post("/createcomment",authorization,comment.createcomment)
-router.patch("/editcomment",authorization,comment.editcomment)
-router.get("/getcommentsforbook/:id",authorization,comment.getcommentsforbook)
-router.delete("/removecomment/:id",authorization,comment.removecomment)
+router.post("/createcomment",comment.createcomment)
+router.patch("/editcomment",comment.editcomment)
+router.get("/getcommentsforbook/:id",comment.getcommentsforbook)
+router.delete("/removecomment/:id",comment.removecomment)
 
 module.exports=router
