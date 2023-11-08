8efac6e983328d033afdcdee76db74802f8bc718
aya ahmed <91829518+aaya-ahmed@users.noreply.github.com>
2023-11-08 20:35:02 +0200

    Update cart.route.js

--- routing/cart.route.js
+++ routing/cart.route.js
@@ -2,9 +2,9 @@ const cartcontroller=require("../controller/cart.controller")
 const authorization = require("../middleware/authorization")
 const router=require("express").Router()
 
-router.post("/addtocart",authorization,cartcontroller.addtocart)
-router.delete("/deletefromcart/:id",authorization,cartcontroller.deletefromcart)
-router.get("/showcart",authorization,cartcontroller.showcart)
-router.post("/buy",authorization,cartcontroller.buy)
+router.post("/addtocart",cartcontroller.addtocart)
+router.delete("/deletefromcart/:id",cartcontroller.deletefromcart)
+router.get("/showcart",cartcontroller.showcart)
+router.post("/buy",cartcontroller.buy)
 
 module.exports=router
