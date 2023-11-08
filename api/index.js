9241ce20a958f3c20272970893b5360b9934c4cd
aaya-ahmed <aaya7098@gmail.com>
2023-11-08 21:43:01 +0200

    update index.json

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config()
-require("../database/connection/connection.js")
+require("./database/connection/connection.js")
 const express=require("express")
 const cors = require("cors")
 
@@ -14,25 +14,25 @@ app.use(express.urlencoded({extended:true}))
 
 
 
-const logroutes=require("../routing/log.route.js")
+const logroutes=require("./routing/log.route.js")
 app.use(logroutes)
 
-const userroutes=require("../routing/user.route.js")
+const userroutes=require("./routing/user.route.js")
 app.use("/user",userroutes)
 
-const categoryroutes=require("../routing/category.route.js")
+const categoryroutes=require("./routing/category.route.js")
 app.use("/category",categoryroutes)
 
-const bookroutes=require("../routing/book.route.js")
+const bookroutes=require("./routing/book.route.js")
 app.use("/book",bookroutes)
 
-const commentroutes=require("../routing/comment.route.js")
+const commentroutes=require("./routing/comment.route.js")
 app.use("/comment",commentroutes)
 
-const cartroutes=require("../routing/cart.route.js")
+const cartroutes=require("./routing/cart.route.js")
 app.use("/cart",cartroutes)
 
-const authroutes=require("../routing/auth.route.js")
+const authroutes=require("./routing/auth.route.js")
 app.use("/auth",authroutes)
 
 app.listen(process.env.port,()=>{console.log("localhost://3000")})
